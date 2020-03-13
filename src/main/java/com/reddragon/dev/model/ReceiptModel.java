693289cfb489582cee7b1cb48d88b651e05768e5
ScorchingShade <ankushors@gmail.com>
2020-03-14 04:44:02 +0530

    Will shell out some changes now for guice test

--- src/main/java/com/reddragon/dev/model/ReceiptModel.java
+++ src/main/java/com/reddragon/dev/model/ReceiptModel.java
@@ -3,12 +3,7 @@ package com.reddragon.dev.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- * @author ankush
- * Mongo collection model for store_info collection
- * uses a builder model
- */
-@Document(collection = "store_info")
+@Document(collection = "receiptmodel")
 public class ReceiptModel {
 
     @Id
